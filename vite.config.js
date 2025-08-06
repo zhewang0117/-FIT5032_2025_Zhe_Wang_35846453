3a3efab8f1f59f24b20db7b6dd45d6c011fe49f5
zhewang0117 <zwan0602@student.monash.edu>
2025-08-06 10:53:17 +0800

    Trigger Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
 
+
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// 使用环境变量判断生产环境
+const isProduction = process.env.NODE_ENV === 'production'
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? '/-FIT5032_2025_Zhe_Wang_35846453/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,9 +21,3 @@ export default defineConfig({
     },
   },
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/-FIT5032_2025_Zhe_Wang_35846453/'
-    : '/'
-}
